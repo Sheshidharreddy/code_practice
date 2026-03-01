76d13551b531d338cb7d1b991634155e55131dbf
Sheshidharreddy <50204181+Sheshidharreddy@users.noreply.github.com>
2026-03-01 19:52:24 +0000

    initial commit

---
+++ Second_Largest_Number /main.java
@@ -0,0 +1,18 @@
+class main{
+
+
+    public static void main(String[] args) {
+ int arr [] = {10, 20, 4, 45, 90, 99}
+ int secondLargetNumber = 0 ;
+ int largetNumber = 0 ;
+  
+ for (int i = 0; i < arr.length ; i++)
+
+    if(a[i]> largetNumber){
+        secondLargetNumber = largetNumber;
+        largetNumber = arr[i]
+    }
+
+    System.System.out.println("Second Larget number " + secondLargetNumber);
+}
+}
