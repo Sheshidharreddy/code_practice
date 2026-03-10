e21e41ee1794a705434ec6aef4f4f1f665e5c067
Sheshidhar1 <Sheshidhars1@gmail.com>
2026-03-09 20:56:29 -0500

    Initial development of the problems

---
+++ Sum_of_Number/src/main/java/org/codePractices/CheckIfNumberIsPrime.java
@@ -0,0 +1,26 @@
+package org.codePractices;
+
+public class CheckIfNumberIsPrime {
+    public static void main(String[] args) {
+        int n = 22;
+        if (isPrime(n)) {
+            System.out.println(n + " is a prime number.");
+        } else {
+            System.out.println(n + " is not a prime number.");
+        }
+    }
+
+    private static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false; // 0 and 1 are not prime numbers
+        }else{
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    return false; // n is divisible by a number other than 1 and itself
+                }
+            }
+            return true; // n is a prime number
+
+        }
+    }
+}
--- Sum_of_Number/src/main/java/org/codePractices/SecondLargestNumber.java
+++ Sum_of_Number/src/main/java/org/codePractices/SecondLargestNumber.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.codePractices;
 
 import java.util.Scanner;
 
