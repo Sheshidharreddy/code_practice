9e22f6c49b39e38dc145812113c8787afc9e96dd
Sheshidhar1 <Sheshidhars1@gmail.com>
2026-03-27 19:58:17 -0500

    basic assignment questions

---
+++ Sum_of_Number/src/main/java/org/codePractices/FindMaxElementInArray.java
@@ -0,0 +1,14 @@
+package org.codePractices;
+
+public class FindMaxElementInArray {
+    public static void main(String args[]) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 4, 12, 45};
+        int maxNumber = arr[0];
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i] > maxNumber) {
+                    maxNumber = arr[i];
+                }
+            }
+        System.out.println("Max number is: " + maxNumber);
+    }
+}
