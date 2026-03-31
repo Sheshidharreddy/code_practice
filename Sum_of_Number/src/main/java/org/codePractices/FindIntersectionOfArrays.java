541454fb6bf64df2b9dcb36921a66ab461afdefe
Sheshidhar1 <Sheshidhars1@gmail.com>
2026-03-31 18:28:14 -0500

    Finding the intersections of two arrays only the unique intersections

---
+++ Sum_of_Number/src/main/java/org/codePractices/FindIntersectionOfArrays.java
@@ -0,0 +1,22 @@
+package org.codePractices;
+
+import java.util.Set;
+
+public class FindIntersectionOfArrays {
+    public static void main(String[] args){
+    int [] arr = {1, 2, 3, 4, 5};
+    int [] arr1 = {4, 5, 6, 7, 8, 4};
+
+        Set<Integer> uniqueElements = new java.util.HashSet<>();
+
+    for (int i = 0; i  < arr.length; i++){
+        for (int j = 0; j < arr1.length; j++){
+            if(arr[i] == arr1[j]){
+                uniqueElements.add(arr[i]);
+
+            }
+        }
+    }
+    System.out.println("Intersection of the two arrays: " + uniqueElements);
+    }
+}
