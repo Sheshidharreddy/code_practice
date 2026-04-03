9431d4b9fdfbe7d0af8f78cc67c64b4ec4ae265b
Sheshidhar1 <Sheshidhars1@gmail.com>
2026-04-02 19:46:42 -0500

    Right rotation of array

---
+++ Sum_of_Number/src/main/java/org/codePractices/LeftAndRightRotationOfArray.java
@@ -0,0 +1,42 @@
+package org.codePractices;
+
+import java.util.Arrays;
+
+
+import java.util.Arrays;
+    public class LeftAndRightRotationOfArray {
+        public static void main(String[] args) {
+            int[] arr = {1, 2, 3, 4, 5, 6};
+            int k = 2;
+
+            rotateLeft(arr, k);
+
+            System.out.println(Arrays.toString(arr));
+        }
+
+        public static void rotateLeft(int[] arr, int k) {
+            int n = arr.length;
+            k = k % n;
+
+            int[] temp = new int[n];
+            int j = 0;
+
+            // Copy from k to end
+            for (int i = k; i < n; i++) {
+                temp[j] = arr[i];
+                j++;
+            }
+
+            // Copy first k elements to end
+            for (int i = 0; i < k; i++) {
+                temp[j] = arr[i];
+                j++;
+            }
+
+            // Copy temp back
+            for (int i = 0; i < n; i++) {
+                arr[i] = temp[i];
+            }
+        }
+    }
+
