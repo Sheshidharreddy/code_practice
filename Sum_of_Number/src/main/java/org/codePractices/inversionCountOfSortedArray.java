1ece4a620c29800adda7345a19e6b873f54acbc1
Sheshidharreddy <reddysheshidharsri31@gmail.com>
2026-08-20 17:40:04 -0500

    Sort the array with count of inversions

--- Sum_of_Number/src/main/java/org/codePractices/inversionCountOfSortedArray.java
+++ Sum_of_Number/src/main/java/org/codePractices/inversionCountOfSortedArray.java
@@ -5,7 +5,7 @@ public class inversionCountOfSortedArray {
     public static void main(String[] args){
 
         inversionCountOfSortedArray s = new inversionCountOfSortedArray();
-        long[] t = {5,4,3,2,1};
+        long[] t = {3,4,3,2,1};
 
         System.out.println(s.inversionCount(t));
 
